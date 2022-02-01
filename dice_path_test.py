ebc85cead86b318b056b54b254f40aceb4117fd5
Jake Valladares <jason.valladares3@gmail.com>
2022-02-01 13:37:01 -0500

    added dice tests

---
+++ dice_path_test.py
@@ -0,0 +1,207 @@
+import numpy as np
+from data import data_adapter as da
+from experiments.test_dice_path import DicePathTestRunner
+from experiments import point_stats, path_stats
+from models import model_utils
+from core import utils
+import dice_ml
+from sklearn.pipeline import Pipeline
+
+import multiprocessing
+import pandas as pd
+from sklearn.model_selection import ParameterGrid
+import sys
+import os
+
+np.random.seed(88557)
+
+NUM_TASKS = 16
+RUN_LOCALLY = True
+SCRATCH_DIR = '/mnt/nfs/scratch1/jasonvallada'
+OUTPUT_DIR = '/home/jasonvallada/corrections_output'
+LOG_DIR = '/home/jasonvallada/MRMC/logs'
+if RUN_LOCALLY:
+    SCRATCH_DIR = '.'
+    OUTPUT_DIR = '../dice_path_output'
+    LOG_DIR = '.'
+
+class ToNumpy:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def transform(self, X):
+        return X.to_numpy()
+
+def test_launcher(p):
+    if RUN_LOCALLY:
+        print("Begin Test")
+    model = p['model_payload']
+    preprocessor = p['preprocessor_payload']
+    dataset = p['dataset_payload']
+
+    d = dice_ml.Data(dataframe=dataset, continuous_features=preprocessor.continuous_features, outcome_name='Y')
+    clf = Pipeline(steps=[('preprocessor', preprocessor),
+                          ('tonumpy', ToNumpy()),
+                          ('classifier', model)])
+    backend = 'sklearn'
+    m = dice_ml.Model(model=clf, backend=backend)
+    dice = dice_ml.Dice(d, m, method="random")
+
+    column_names_per_feature = [] # list of lists
+    for feature in dataset.columns.difference(['Y']):
+        column_names_per_feature.append(preprocessor.get_feature_names_out([feature]))
+
+    experiment_immutable_feature_names = None
+    if p['dataset'] == 'adult_income':
+        experiment_immutable_feature_names = ['age', 'race', 'sex']
+    elif p['dataset'] == 'german_credit':
+        experiment_immutable_feature_names = ['age', 'sex']
+
+    path_statistics = {
+        'Path Invalidity': lambda paths: path_stats.check_validity_distance(preprocessor, paths),
+        'Path Count': path_stats.check_path_count,
+        'Path Length': path_stats.check_path_length,
+        'Path Immutable Violations': lambda paths: path_stats.check_immutability(preprocessor, experiment_immutable_feature_names, paths),
+        'Average Path Sparsity': lambda paths: path_stats.check_sparsity(preprocessor, paths),
+        'Path Invalidity': lambda paths: path_stats.check_validity(preprocessor, column_names_per_feature, paths),
+    }
+    point_statistics = {
+        'Positive Probability': lambda poi, paths: point_stats.check_positive_probability(model, poi, paths, p['certainty_cutoff']),
+        'Point Invalidity': lambda poi, points: point_stats.check_validity(preprocessor, column_names_per_feature, poi, points),
+        'Final Point Distance': point_stats.check_final_point_distance,
+        'Point Immutable Violations': lambda poi, points: point_stats.check_immutability(preprocessor, experiment_immutable_feature_names, poi, points),
+        'Point Sparsity': lambda poi, points: point_stats.check_sparsity(preprocessor, poi, points),
+        'Diversity': point_stats.check_diversity,
+        'Model Certainty': lambda poi, cf_points: point_stats.check_model_certainty(model, poi, cf_points)
+    }
+
+    k_paths = p['k_paths']
+    immutable_features = None
+    feature_tolerances = None
+    if p['immutable_features']:
+        if p['dataset'] == 'adult_income':
+            immutable_features = ['age', 'race', 'sex']
+        if p['dataset'] == 'german_credit':
+            immutable_features = ['age', 'sex']
+        feature_tolerances = {'age': 5}
+    
+    max_iterations = p['max_iterations']
+
+    weight_function = lambda dir: dir * p['weight_function_alpha']
+    perturb_dir = None
+    if p['perturb_dir_random_scale'] is not None:
+        perturb_dir = lambda dir: utils.random_perturb_dir(p['perturb_dir_random_scale'], dir)
+
+    num_trials = p['num_trials']
+    certainty_cutoff = p['certainty_cutoff']
+    test = DicePathTestRunner(num_trials,
+                              dataset,
+                              preprocessor,
+                              dice,
+                              point_statistics,
+                              path_statistics,
+                              k_paths,
+                              certainty_cutoff,
+                              max_iterations,
+                              weight_function,
+                              clf,
+                              perturb_dir=perturb_dir,
+                              immutable_features=immutable_features,
+                              feature_tolerances=feature_tolerances)
+
+    _, aggregated_stats = test.run_test()
+    return aggregated_stats
+
+
+def get_params(num_trials, dataset_str):
+    """Create a dataframe of input parameters.
+    
+    Each row of the dataframe contains a setting over parameters used by test_launcher to launch a test.
+    Grid search is performed over these parameters, testing every combination of values for all the parameters.
+    Simple parameters can be searched over naively. Parameters like early stopping are handled more carefully --
+    if early stopping is disabled (early_stopping = False), there is no point in searching over early_stopping_cutoff.
+    """
+
+    # simple parameters have no conflicts
+    params = {
+        'num_trials': [num_trials],
+        'max_iterations': [15],
+        'weight_function_alpha': [0.7],
+        'perturb_dir_random_scale': [None, 0.25, 0.5, 1, 2, 4],
+        'k_paths': [4],
+        'model': ['svc', 'random_forest'],
+        'immutable_features': [True],
+        'dataset': [dataset_str],
+        'certainty_cutoff': [0.6, 0.7]
+    }
+    
+    return list(ParameterGrid(params))
+
+def write_dataframe(params_df, results_dataframe_list, output_file):
+    results_dataframe = pd.concat(results_dataframe_list, axis=0).reset_index()
+    results_dataframe = results_dataframe
+    final_df = pd.concat([params_df.reset_index(), results_dataframe], axis=1)
+    final_df.to_pickle(output_file)
+    if RUN_LOCALLY:
+        outputs = [
+            'model',
+            'perturb_dir_random_scale',
+            'certainty_cutoff',
+            'Positive Probability (mean)',
+            #'Model Certainty (mean)',
+            'Final Point Distance (mean)',
+            'Path Count (mean)',
+            #'Point Sparsity (mean)',
+        ]
+        print(final_df[outputs])
+
+def run_experiment():
+    print("starting the script...")
+    args = sys.argv
+    dataset = None
+    num_tests = int(args[2])
+    dataset = args[1]
+    print("dataset is ", dataset)
+    output_file = os.path.join(OUTPUT_DIR, f'{dataset}.pkl')
+    num_trials = 30
+
+    models = {
+        ('svc', 'german_credit'): model_utils.load_model('svc', 'german_credit'),
+        ('svc', 'adult_income'): model_utils.load_model('svc', 'adult_income'),
+        ('random_forest', 'german_credit'): model_utils.load_model('random_forest', 'german_credit'),
+        ('random_forest', 'adult_income'): model_utils.load_model('random_forest', 'adult_income'),
+    }
+
+    german_data, _, german_preprocessor = da.load_german_credit_dataset()
+    adult_data, _, adult_preprocessor = da.load_adult_income_dataset()
+
+    all_params = get_params(num_trials, dataset)
+    print(len(all_params))
+    if num_tests == 0:
+        num_tests = len(all_params)
+    print(f"Run {num_tests} tests")
+    params = all_params[:num_tests]
+    for param_dict in params:
+        new_params = {
+            'seed': np.random.randint(999999),
+            'dataset_payload': german_data if param_dict['dataset'] == 'german_credit' else adult_data,
+            'preprocessor_payload': german_preprocessor if param_dict['dataset'] == 'german_credit' else adult_preprocessor,
+            'model_payload': models[(param_dict['model'], param_dict['dataset'])]
+        }
+        param_dict.update(new_params)
+
+    results = None
+    with multiprocessing.Pool(NUM_TASKS) as p:
+        results = p.map(test_launcher, params)
+
+    param_df = pd.DataFrame(params).drop(['dataset_payload', 'preprocessor_payload', 'model_payload'], axis=1)
+
+    write_dataframe(param_df, results, output_file)
+    print("Finished experiment.")
+
+
+if __name__ == '__main__':
+    run_experiment()
---
+++ experiments/test_dice_path.py
@@ -0,0 +1,129 @@
+from data import data_adapter as da
+import numpy as np
+import pandas as pd
+import itertools
+
+
+class DicePathTestRunner:
+    def __init__(self, N, dataset, preprocessor, dice,
+                 point_statistics, path_statistics, k_points, 
+                 certainty_cutoff, max_iterations, 
+                 weight_function, clf, perturb_dir=None, immutable_features=None, 
+                 feature_tolerances=None):
+        self.point_statistics = point_statistics
+        self.path_statistics = path_statistics
+        self.statistics_keys = list(path_statistics.keys()) + list(point_statistics.keys())
+        self.dice = dice
+        self.dataset = dataset
+        self.certainty_cutoff = certainty_cutoff
+        self.N = N
+        self.k_points = k_points
+        self.feature_tolerances = feature_tolerances
+        self.perturb_dir = perturb_dir
+        self.max_iterations = max_iterations
+        self.preprocessor = preprocessor
+        self.weight_function = weight_function
+        self.clf = clf
+        if immutable_features is not None:
+            self.features_to_vary = list(dataset.columns.difference(immutable_features + ['Y']))
+            if feature_tolerances is not None:
+                self.features_to_vary += list(feature_tolerances.keys())
+        else:
+            self.features_to_vary = list(dataset.columns.difference(['Y']))
+
+    def iterate_dice(self, poi, first_cf, permitted_range, max_iterations):
+        old_poi = self.preprocessor.transform(poi)
+        path = old_poi.reset_index(drop=True)
+        new_cf = first_cf
+        for i in range(max_iterations):
+            new_cf_processed = self.preprocessor.transform(new_cf)
+            dir = new_cf_processed.to_numpy() - old_poi.to_numpy()
+            dir = self.weight_function(dir)
+            if self.perturb_dir is not None:
+                dir = self.perturb_dir(dir)
+            curr_poi = old_poi + dir
+            if np.isinf(curr_poi).any().any():
+                return path
+            if np.isnan(curr_poi).any().any():
+                return path
+            path = path.append(curr_poi, ignore_index=True)
+            curr_poi = self.preprocessor.inverse_transform(curr_poi)
+            model_certainty = self.clf.predict_proba(curr_poi)[0,1]
+            if model_certainty >= self.certainty_cutoff:
+                return path
+
+            desired_class = "opposite"
+            if self.clf.predict_proba(curr_poi)[0,1] >= 0.5:
+                desired_class = 1
+            new_cf = self.dice.generate_counterfactuals(
+                curr_poi, 
+                total_CFs=1,
+                desired_class=desired_class,
+                features_to_vary=self.features_to_vary,
+                permitted_range=permitted_range,
+                random_seed=88557,
+                stopping_threshold=self.certainty_cutoff).cf_examples_list[0].final_cfs_df
+            new_cf = self.preprocessor.transform(new_cf.drop('Y', axis=1))
+        return path
+
+    def run_trial(self):
+        poi = da.random_poi(self.dataset)
+        permitted_range = {}
+        if self.feature_tolerances is not None:
+            for key, val in self.feature_tolerances.items():
+                poi_val = poi.loc[poi.index[0],key]
+                permitted_range[key] = [poi_val - val, poi_val + val]
+
+        cf_points = self.dice.generate_counterfactuals(
+            poi,
+            total_CFs=self.k_points,
+            desired_class="opposite",
+            features_to_vary=self.features_to_vary,
+            permitted_range=permitted_range,
+            random_seed=88557,
+            stopping_threshold=self.certainty_cutoff).cf_examples_list[0].final_cfs_df
+        
+        paths = []
+        for i in cf_points.index:
+            cf = cf_points.loc[cf_points.index == i,:].drop('Y', axis=1)
+            path = self.iterate_dice(poi, cf, permitted_range, self.max_iterations - 1)
+            paths.append(path)
+        return self.collect_statistics(poi, paths), paths, poi
+
+    def collect_statistics(self, poi, paths):
+        stat_dict = {}
+        for statistic, calculate_statistic in self.path_statistics.items():
+            stat_dict[statistic] = calculate_statistic(paths)
+        for statistic, calculate_statistic in self.point_statistics.items():
+            points = pd.DataFrame(columns=paths[0].columns, data=np.zeros((len(paths), len(paths[0].columns))))
+            for i in range(len(paths)):
+                points.iloc[i,:] = paths[i].iloc[-1,:]
+            stat_dict[statistic] = calculate_statistic(self.preprocessor.transform(poi), points)
+        return stat_dict
+
+    def run_test(self):
+        stats_dict = dict([(stat_key, np.full(self.k_points*self.N, np.nan)) for stat_key in self.statistics_keys])
+        for n in range(self.N):
+            i = n*self.k_points
+            j = (n+1)*self.k_points
+            stats, _, _ = self.run_trial()
+            for key in self.statistics_keys:
+                stats_dict[key][i:j] = stats[key]
+        stats = pd.DataFrame(stats_dict)
+        aggregated_statistics = self.aggregate_stats(stats)
+        return stats, aggregated_statistics
+
+    def aggregate_stats(self, statistics):
+        non_null_stats = statistics[~statistics.isnull().any(axis=1)]
+        non_null_ratio = non_null_stats.shape[0] / statistics.shape[0]
+        meta_stats = ['mean', 'median', 'min', 'max', 'std']
+        described_stats = non_null_stats.describe()
+        described_stats.loc['median',:] = non_null_stats.median()
+        aggregated_columns = [f'{metric} ({stat})' for metric, stat in itertools.product(statistics.columns, meta_stats)]
+        aggregated_stats = pd.DataFrame(columns=aggregated_columns, data=np.zeros(shape=(1,len(aggregated_columns))))
+        
+        for metric, stat in itertools.product(statistics.columns, meta_stats):
+            aggregated_stats[f'{metric} ({stat})'] = described_stats.loc[stat, metric]
+
+        aggregated_stats.loc[:,'success_ratio'] = non_null_ratio
+        return aggregated_stats
