88410528020146941e555be913a3e808b3d9099f
Jake Valladares <jason.valladares3@gmail.com>
2022-02-01 10:35:39 -0500

    cleaned & commented face

--- face/core.py
+++ face/core.py
@@ -54,12 +54,42 @@ class Face:
         self.preprocessor = None
         self.original_graph = None
         self.original_density_scores = None
-        self.recover_originals = False
 
     def clean_number(f):
         return ''.join(map(lambda c: '-' if c == '.' else c, f'{f:.3f}'))
 
+    def write_kde_to_file(preprocessor, dataset, dataset_str, bandwidth, rtol=None, dir='./face_graphs'):
+        """Performs density estimation on a dataset and saves it to the filesystem.
+        """
+        X = preprocessor.transform(dataset)
+        if 'Y' in X.columns:
+            X = X.drop('Y', axis=1)
+        X = X.to_numpy()
+
+        bandwidth_str = Face.clean_number(bandwidth)
+        density_path = None
+        if rtol is not None:
+            rtol_str = Face.clean_number(rtol)
+            density_path = os.path.join(dir, f'{dataset_str}_density_scores_{bandwidth_str}_rtol_{rtol_str}.npy')
+        else:
+            density_path = os.path.join(dir, f'{dataset_str}_density_scores_{bandwidth_str}.npy')
+        kde = None
+        if kde is not None:
+            kde = KernelDensity(bandwidth=bandwidth, rtol=rtol)
+        else:
+            kde = KernelDensity(bandwidth=bandwidth)        
+        kde.fit(X)
+        density_estimator = lambda Z: np.exp(kde.score_samples(Z))
+        density_scores = density_estimator(X)
+        np.save(density_path, density_scores)
+
+
     def set_kde(self, preprocessor, dataset, dataset_str, bandwidth, rtol=None, dir='./face_graphs', save_results=True):
+        """Sets the KDE scores with a given bandwidth on the given dataset.
+
+        If the results have been pre-computed and cached in the filesystem, it loads those automatically.
+        Otherwise it calculates the results and saves them.
+        """
         X = preprocessor.transform(dataset)
         if 'Y' in X.columns:
             X = X.drop('Y', axis=1)
@@ -86,29 +116,13 @@ class Face:
             if save_results:
                 np.save(density_path, self.density_scores)
 
-    def set_kde_subset(self, preprocessor, dataset, dataset_str, bandwidth, idx, rtol=None, dir='./face_graphs'):
-        X = preprocessor.transform(dataset)
-        if 'Y' in X.columns:
-            X = X.drop('Y', axis=1)
-        X = X.to_numpy()
-
-        bandwidth_str = Face.clean_number(bandwidth)
-        density_path = None
-        if rtol is not None:
-            rtol_str = Face.clean_number(rtol)
-            density_path = os.path.join(dir, f'{dataset_str}_density_scores_{bandwidth_str}_rtol_{rtol_str}.npy')
-        else:
-            density_path = os.path.join(dir, f'{dataset_str}_density_scores_{bandwidth_str}.npy')
-        kde = None
-        if kde is not None:
-            kde = KernelDensity(bandwidth=bandwidth, rtol=rtol)
-        else:
-            kde = KernelDensity(bandwidth=bandwidth)
-        kde.fit(X)
-        self.density_estimator = lambda Z: np.exp(kde.score_samples(Z))
-        self.density_scores = np.load(density_path)[idx]
-
     def get_num_blocks(preprocessor, dataset, block_size=80000000):
+        """Calculates the number of blocks the dataset will be split into when generating the graph.
+
+        Datasets are split into blocks because the full adjacency matrix graph does not fit in memory.
+        Instead, it is calculated in blocks and distant edges are removed, allowing for a compressed
+        representation with scipy.sparse.
+        """
         X = preprocessor.transform(dataset)
         if 'Y' in X.columns:
             X = X.drop('Y', axis=1)
@@ -118,73 +132,21 @@ class Face:
         num_blocks = int(np.ceil(X.shape[0] / rows_per_block))
         return num_blocks
 
-    def generate_graph_block(self, preprocessor, dataset, 
-                             block_index, bandwidth, 
-                             block_size=80000000, dir='./face_graphs'):
-        X = preprocessor.transform(dataset)
-        if 'Y' in X.columns:
-            X = X.drop('Y', axis=1)
-        X = X.to_numpy()
-
-        kde = KernelDensity(bandwidth=bandwidth)
-        kde.fit(X)
-        density_estimator = lambda Z: np.exp(kde.score_samples(Z))
-
-        rows_per_block = int(np.floor(block_size / (X.shape[1] * X.shape[0])))
-
-        block_start = block_index*rows_per_block
-        block_end = (block_index+1)*rows_per_block
-        
-        # calculate the (N x N x D) pairwise difference matrix
-        differences = X[block_start:block_end,None] - X
-
-        # calculate the (N x N) conditions mask
-        conditions_mask = np.ones((differences.shape[0], differences.shape[1])).astype(np.bool)
-        if self.conditions_function is not None:
-            conditions_mask = self.conditions_function(differences)
-
-        # calculate the distances matrix
-        distances = np.linalg.norm(differences, axis=2)
-        
-        # calculate the neighbor mask
-        neighbor_mask = ~((distances > self.distance_threshold) | ~conditions_mask) # true wherever there's an edge
-        distances[~neighbor_mask] = 0.0
-
-        # calculate the weighted densities
-        # num_rows = block_end - block_start
-        midpoints = ((X[block_start:block_end,None] + X) / 2)[neighbor_mask]
-        density = np.zeros((distances.shape[0], X.shape[0]))
-        density[neighbor_mask] = self.weight_function(density_estimator(midpoints))
-
-        graph_block = sparse.coo_matrix(distances * density)
-        return graph_block
-
-    def set_graph_from_blocks(self, graph_blocks, preprocessor, dataset, dataset_str, bandwidth, rtol=None, dir='./face_graphs'):
-        graph = None
-        for block in graph_blocks:
-            if graph is None:
-                graph = block
-            else:
-                graph = sparse.vstack([graph, block])
-
-        X = preprocessor.transform(dataset)
-        if 'Y' in X.columns:
-            X = X.drop('Y', axis=1)
-        X = X.to_numpy()
-
-        bandwidth_str = Face.clean_number(bandwidth)
-        distance_str = Face.clean_number(self.distance_threshold)
-        graph_path = None
-        if rtol is not None:
-            rtol_str = Face.clean_number(rtol)
-            graph_path = os.path.join(dir, f'{dataset_str}_density_{bandwidth_str}_rtol_{rtol_str}_conditions_{self.conditions_function is not None}_distance_{distance_str}_.npz')
-        else:
-            graph_path = os.path.join(dir, f'{dataset_str}_density_{bandwidth_str}_conditions_{self.conditions_function is not None}_distance_{distance_str}_.npz')
-
-        self.graph = graph
-        sparse.save_npz(graph_path, self.graph)
-
     def set_graph(self, preprocessor, dataset, dataset_str, bandwidth, block_size=80000000, rtol=None, dir='./face_graphs', save_results=True):
+        """Set the FACE graph.
+
+        If the graph has been pre-computed and cached in the filesystem, it loads those automatically.
+        Otherwise it calculates the results and saves them.
+
+        An approximation is used to speed computation, bringing it from O(N^3) (KDE on all midpoints) to O(N) (using cached KDE score on min-density point in a pair).
+        The difference is negligible for synthetic datasets. The difference on real-world datasets is untested.
+
+        The graph is calculated because the full adjacency matrix graph does not fit in memory.
+        Instead, it is calculated in blocks and distant edges are removed, allowing for a compressed
+        representation with scipy.sparse.
+        """
+
+        # get the KDE estimation
         if self.density_scores is None:
             self.set_kde(preprocessor, dataset, dataset_str, bandwidth, rtol=rtol, dir=dir, save_results=save_results)
 
@@ -193,6 +155,7 @@ class Face:
             X = X.drop('Y', axis=1)
         X = X.to_numpy().astype(np.float32)
 
+        # attempt to load the graph from the filesystem
         bandwidth_str = Face.clean_number(bandwidth)
         distance_str = Face.clean_number(self.distance_threshold)
         graph_path = None
@@ -205,6 +168,7 @@ class Face:
             self.graph = sparse.load_npz(graph_path)
             return
 
+        # split the dataset into blocks and begin calculation
         rows_per_block = int(np.floor(block_size / (X.shape[1] * X.shape[0])))
         num_blocks = int(np.ceil(X.shape[0] / rows_per_block))
         graph = None
@@ -228,7 +192,6 @@ class Face:
             distances[~neighbor_mask] = 0.0
 
             # calculate the weighted densities
-            # num_rows = block_end - block_start
             density = None
             if LINEAR_APPROXIMATION:
                 # Instead of using the density of the midpoint of all points, choose the minimum endpoint density
@@ -255,6 +218,8 @@ class Face:
             sparse.save_npz(graph_path, self.graph)
 
     def load_graph(dataset, bandwidth, distance_threshold, use_conditions, rtol=None, dir='./face_graphs'):
+        """Load the graph and density scores from the filesystem.
+        """
         bandwidth_str = Face.clean_number(bandwidth)
         density_path = None
         if rtol is not None:
@@ -271,14 +236,14 @@ class Face:
             graph_path = os.path.join(dir, f'{dataset}_density_{bandwidth_str}_rtol_{rtol_str}_conditions_{use_conditions}_distance_{distance_str}_.npz')
         else:
             graph_path = os.path.join(dir, f'{dataset}_density_{bandwidth_str}_conditions_{use_conditions}_distance_{distance_str}_.npz')
-        graph = sparse.load_npz(graph_path)
 
+        graph = sparse.load_npz(graph_path)
         return density_scores, graph
 
     def set_graph_from_memory(self, graph, density_scores):
         self.graph = graph
         self.density_scores = density_scores
-        
+
     def fit(self, dataset, preprocessor, verbose=False):
         X = preprocessor.transform(dataset)
         if 'Y' in X.columns:
@@ -287,21 +252,14 @@ class Face:
         self.dataset = dataset
         self.X = X
         self.preprocessor = preprocessor
-        if self.recover_originals:
-            self.density_scores = self.original_density_scores
-            self.graph = self.original_graph
-            self.recover_originals = False
         self.candidate_mask = (self.clf(self.X) >= self.confidence_threshold) & (self.density_scores >= self.density_threshold)
-        if verbose:
-            mask = (self.density_scores >= self.density_threshold)
-            total = mask.shape[0]
-            culled = mask.shape[0] - mask[mask].shape[0]
-            mask = (self.clf(self.X) >= self.confidence_threshold)
-            total = mask.shape[0]
-            culled = mask.shape[0] - mask[mask].shape[0]
 
     def add_age_condition(self, age_tolerance, poi_index):
-        self.recover_originals = True
+        """Culls points from the graph which would violate immutability constraints.
+
+        The paper poses this functionality as a function (self.conditions_function), but
+        it is easier and more efficient to achieve the desired effect via filtering.
+        """
         self.original_graph = self.graph
         self.original_density_scores = self.density_scores
         poi_age = self.dataset.loc[poi_index, 'age']
@@ -311,18 +269,27 @@ class Face:
         self.candidate_mask = self.candidate_mask[age_mask]
         self.graph = self.graph.tocsr()[age_mask][:,age_mask].tocoo()
 
+    def clear_age_condition(self):
+        if self.original_density_scores is not None:
+            self.density_scores = self.original_density_scores
+            self.graph = self.original_graph
+
     def iterate(self, poi_index):
-        # graph = sparse.csgraph.csgraph_from_dense(self.graph)
+        """Perform dijkstra's search on the graph.
+        """
+
+        # perform the search
         poi_index = self.dataset.index.get_loc(poi_index)
         csgraph = self.graph.tocsr()
-        # print(csgraph)
         dist_matrix, predecessors = sparse.csgraph.dijkstra(csgraph, indices=poi_index, return_predecessors=True)
 
+        # find the nearest candidate points
         candidates = np.arange(self.X.shape[0])[self.candidate_mask]
         k = min(self.k_paths, candidates.shape[0])
         sorted_indices = np.argpartition(dist_matrix[self.candidate_mask], k-1)[:k]
         cf_idx = candidates[sorted_indices]
 
+        # reconstruct and return the paths
         processed_data = None
         if 'Y' in self.dataset.columns:
             processed_data = self.preprocessor.transform(self.dataset).drop('Y', axis=1)
@@ -338,56 +305,6 @@ class Face:
             while point != -9999:
                 path.append(self.X[point])
                 point = predecessors[point]
-            pathdf = pd.DataFrame(columns=columns, data=path)
+            pathdf = pd.DataFrame(columns=columns, data=path).loc[::-1].reset_index(drop=True)
             paths.append(pathdf)
-        return paths
-
-    def iterate2(self, poi_index):
-        indices = np.arange(0, self.X.shape[0])
-        distances = np.full(self.X.shape[0], np.inf)
-        distances[poi_index] = 0
-        frontier = list([(distance, i) for distance, i in zip(distances, range(self.X.shape[0]))])
-        frontier_set = np.ones(len(frontier)).astype(np.bool)
-        heapq.heapify(frontier)
-        parents = np.full(self.X.shape[0], -1)
-        while len(frontier) > 0:
-            curr_idx = None
-            end_loop = False
-            while curr_idx is None:
-                if len(frontier) == 0:
-                    end_loop = True
-                    break
-                _, curr_idx = heapq.heappop(frontier)
-                if not frontier_set[curr_idx]:
-                    curr_idx = None
-            if end_loop:
-                break
-            frontier_set[curr_idx] = False
-            new_distances = self.graph[curr_idx]
-            curr_distance = distances[curr_idx]
-            distance_mask = distances > curr_distance + new_distances
-            neighbor_mask = (self.graph[curr_idx] > 0) & frontier_set
-            mask = distance_mask & neighbor_mask
-
-            for i in indices[mask]:
-                distances[i] = curr_distance + new_distances[i]
-                parents[i] = curr_idx
-                heapq.heappush(frontier, (curr_distance + new_distances[i], i))
-        
-        candidates = np.arange(self.X.shape[0])[self.candidate_mask]
-        k = min(self.k_paths, candidates.shape[0])
-        sorted_indices = np.argpartition(distances[self.candidate_mask], k-1)[:k]
-        cf_idx = candidates[sorted_indices]
-
-        paths = []
-        for final_point in cf_idx:
-            path = np.array([])
-            point = final_point
-            if distances[point] == np.inf:
-                continue
-            while point != -1:
-                path = np.append(point, path)
-                point = parents[point]
-            paths.append(path.astype(np.int32))
-        
         return paths
