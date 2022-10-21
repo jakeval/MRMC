a8ac0456361beddb152bf5e8c6b5b8a3a215132a
Jake Valladares <jason.valladares3@gmail.com>
2022-10-21 19:10:23 -0400

    updated remaining files to reflect changes

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,33 +1,45 @@
 import numpy as np
 import pandas as pd
-from typing import Tuple, Optional
-from data.credit_card_default import credit_card_default_loader
-from data import recourse_adapter
+from typing import Optional
+from data.datasets import credit_card_default_loader
 
 
-def load_credit_card_default_dataset(only_continuous: bool = True, data_dir: Optional[str] = None) -> Tuple[pd.DataFrame, recourse_adapter.RecourseAdapter]:
-    """Returns the training data and a recourse adapter for the Credit Card Default dataset.
+def load_credit_card_default_dataset(
+    only_continuous: bool = True, data_dir: Optional[str] = None
+) -> pd.DataFrame:
+    """Returns the training data for the Credit Card Default dataset.
 
     Args:
-        only_continuous: Whether to drop or recategorize categorical variables, leaving only continuous features.
-        data_dir: An override for the directory to load the data from (or download it to if unavailable).
+        only_continuous: Whether to drop or recategorize categorical variables,
+            leaving only continuous features.
+        data_dir: An override for the directory to load the data from (or
+            download it to if unavailable).
 
     Returns:
-        The Credit Card Default dataset and a fitted recourse adapter."""
+        The Credit Card Default dataset."""
     if data_dir is not None:
-        return credit_card_default_loader.load_data(only_continuous=only_continuous, data_dir=data_dir)
+        return credit_card_default_loader.load_data(
+            only_continuous=only_continuous, data_dir=data_dir
+        )
     else:
-        return credit_card_default_loader.load_data(only_continuous=only_continuous)
+        return credit_card_default_loader.load_data(
+            only_continuous=only_continuous
+        )
 
 
 # TODO(@jakeval): This will be removed in a later PR
-def filter_from_poi(dataset, poi, immutable_features=None, feature_tolerances=None):
+def filter_from_poi(
+    dataset, poi, immutable_features=None, feature_tolerances=None
+):
     df = dataset[dataset.index != poi.index[0]]
     if immutable_features is not None:
         for feature in immutable_features:
             mask = None
-            feature_value = poi.loc[poi.index[0],feature]
-            if feature_tolerances is not None and feature in feature_tolerances:
+            feature_value = poi.loc[poi.index[0], feature]
+            if (
+                feature_tolerances is not None
+                and feature in feature_tolerances
+            ):
                 tol = feature_tolerances[feature]
                 mask = np.abs(df[feature] - feature_value) <= tol
             else:
@@ -38,7 +50,11 @@ def filter_from_poi(dataset, poi, immutable_features=None, feature_tolerances=No
 
 # TODO(@jakeval): This will be removed in a later PR
 def filter_from_model(dataset, model_scores, certainty_cutoff=0.7):
-    Y_indices = dataset.Y.mask(dataset.Y == -1, 0).to_numpy().astype('int32') # 0-1 representation
-    correct_proba = np.where(Y_indices, model_scores[:,1], model_scores[:,0]) # the probability of the correct label
+    Y_indices = (
+        dataset.Y.mask(dataset.Y == -1, 0).to_numpy().astype("int32")
+    )  # 0-1 representation
+    correct_proba = np.where(
+        Y_indices, model_scores[:, 1], model_scores[:, 0]
+    )  # the probability of the correct label
     high_certainty_idx = correct_proba >= certainty_cutoff
     return dataset[high_certainty_idx]
