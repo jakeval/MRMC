6533e0c4ff964e444f844ea7666d29ed8a261aaf
Jake Valladares <jason.valladares3@gmail.com>
2022-10-21 14:46:28 -0400

    split the recourse_adapter.py code into multiple files

---
+++ data/adapters/categorical_adapter.py
@@ -0,0 +1,204 @@
+from __future__ import annotations
+from data import recourse_adapter
+from typing import Sequence, Optional, Mapping
+from core import utils
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+import pandas as pd
+
+
+class OneHotAdapter(recourse_adapter.RecourseAdapter):
+    """A recourse adapter which one-hot encodes categorical variables and
+    standardizes continuous data to have mean 0 and standard deviation 1.
+
+    The adapter also optionally simulates rescaling the recourse or adding
+    random noise while interpreting recourse instructions."""
+
+    def __init__(
+        self,
+        categorical_features: Sequence[str],
+        continuous_features: Sequence[str],
+        perturb_ratio: Optional[float] = None,
+        rescale_ratio: Optional[float] = None,
+        label="Y",
+    ):
+        """Creates a new OneHotAdapter.
+
+        Args:
+            categorical_features: The names of the categorical features.
+            continuous_features: The names of the continuous features.
+            perturb_ratio: The magnitude of random noise relative to the
+                recourse directions to add while interpreting recourse
+                instructions.
+            rescale_ratio: The amount to rescale the recourse directions by
+                while interpreting recourse instructions.
+            label: The name of the class label feature."""
+        self.categorical_features = categorical_features
+        self.continuous_features = continuous_features
+        self.label = label
+
+        self.sc_dict: Mapping[str, StandardScaler] = None
+        self.ohe_dict: Mapping[str, OneHotEncoder] = None
+        self.columns = None
+        self.perturb_ratio = perturb_ratio
+        self.rescale_ratio = rescale_ratio
+
+    def get_label(self) -> str:
+        """Gets the dataset's label column name."""
+        return self.label
+
+    def fit(self, dataset: pd.DataFrame) -> OneHotAdapter:
+        """Fits the adapter to a dataset.
+
+        Args:
+            dataset: The data to fit.
+
+        Returns:
+            Itself. Fitting is done mutably."""
+        self.sc_dict = {}
+        self.ohe_dict = {}
+        self.columns = dataset.columns
+        for feature in self.continuous_features:
+            sc = StandardScaler()
+            sc.fit(dataset[[feature]])
+            self.sc_dict[feature] = sc
+        for feature in self.categorical_features:
+            ohe = OneHotEncoder()
+            ohe.fit(dataset[[feature]])
+            self.ohe_dict[feature] = ohe
+        return self
+
+    def transform(
+        self, dataset: pd.DataFrame
+    ) -> recourse_adapter.EmbeddedDataFrame:
+        """Transforms data from human-readable format to an embedded continuous
+        space by standardizing the data and one-hot encoding categorical vars.
+
+        Args:
+            dataset: The data to transform.
+
+        Returns:
+            Transformed data."""
+        df = dataset.copy()
+        for feature in self.continuous_features:
+            if feature in df.columns:
+                df[feature] = self.sc_dict[feature].transform(df[[feature]])
+        for feature in self.categorical_features:
+            if feature in df.columns:
+                ohe = self.ohe_dict[feature]
+                feature_columns = ohe.get_feature_names_out([feature])
+                df[feature_columns] = ohe.transform(df[[feature]]).toarray()
+                df = df.drop(feature, axis=1)
+        return df
+
+    def inverse_transform(
+        self, dataset: recourse_adapter.EmbeddedDataFrame
+    ) -> pd.DataFrame:
+        """Transforms data from an embedded continuous space to its original
+        human-readable format.
+
+        Args:
+            dataset: The data to inverse transform.
+
+        Returns:
+            Inverse transformed data."""
+        df = dataset.copy()
+        for feature in self.continuous_features:
+            if feature in df.columns:
+                df[feature] = self.sc_dict[feature].inverse_transform(
+                    df[[feature]]
+                )
+        for feature in self.categorical_features:
+            ohe = self.ohe_dict[feature]
+            feature_columns = ohe.get_feature_names_out([feature])
+            if df.columns.intersection(feature_columns).any():
+                df[feature] = ohe.inverse_transform(df[feature_columns])
+                df = df.drop(feature_columns, axis=1)
+        return df
+
+    def directions_to_instructions(
+        self, directions: recourse_adapter.EmbeddedSeries
+    ) -> recourse_adapter.EmbeddedSeries:
+        """Converts a direction in embedded space to a human-readable
+        instructions format.
+
+        For this class, it is a no-op.
+
+        Args:
+            directions: The continuous recourse directions to convert.
+
+        Returns:
+            Human-readable instructions describing the recourse directions."""
+        return directions
+
+    def interpret_instructions(
+        self, poi: pd.Series, instructions: recourse_adapter.EmbeddedSeries
+    ) -> pd.Series:
+        """Returns a new human-readable data point from an original Point of
+        Interest (POI) and set of recourse instructions.
+
+        Converts the POI to the embedded space and translates it using the
+        embedded space instructions. Then reconverts the POI to its original
+        data space.
+
+        If self.perturb_ratio is not None, adds random noise while translating
+        the POI.
+
+        If self.rescale_ratio is not None, rescales the magnitude of the
+        translation.
+
+        Args:
+            poi: The point of interest (POI) to translate.
+            instructions: The recourse instructions to interpret.
+
+        Returns:
+            A new POI translated from the original by the recourse
+            instructions."""
+        if self.perturb_ratio:
+            instructions = utils.randomly_perturb_dir(
+                instructions, self.perturb_ratio
+            )
+        if self.rescale_ratio:
+            instructions = utils.rescale_dir(instructions, self.rescale_ratio)
+        poi = self.transform_series(poi)
+        cfe = poi + instructions
+        return self.inverse_transform_series(cfe)
+
+    def column_names(self, drop_label=True) -> Sequence[str]:
+        """Returns the column names of the human-readable data.
+
+        Args:
+            drop_label: Whether the label column should be excluded in the
+                output.
+
+        Returns:
+            A list of the column names."""
+        if drop_label:
+            return self.columns.difference([self.label])
+        else:
+            return self.columns
+
+    def embedded_column_names(self, drop_label=True) -> Sequence[str]:
+        """Returns the column names of the data in embedded continuous space.
+
+        Args:
+            drop_label: Whether the label column should be excluded in the
+                output.
+
+        Returns:
+            A list of the column names."""
+        columns = self._get_feature_names_out(self.columns)
+        if drop_label:
+            return [column for column in columns if column != self.label]
+        else:
+            return columns
+
+    def _get_feature_names_out(self, features: Sequence[str]) -> Sequence[str]:
+        features_out = []
+        for feature in features:
+            if feature in self.categorical_features:
+                features_out += list(
+                    self.ohe_dict[feature].get_feature_names_out([feature])
+                )
+            else:
+                features_out.append(feature)
+        return features_out
--- data/recourse_adapter.py
+++ data/recourse_adapter.py
@@ -1,13 +1,15 @@
-from __future__ import annotations
-from typing import Any, Sequence, Mapping, Optional
+from typing import Any, Sequence
 import pandas as pd
 import abc
-from core import utils
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 
 class EmbeddedDataFrame(pd.DataFrame):
-    """A wrapper around DataFrame for purely numeric data."""
+    """A wrapper around DataFrame for continuous data.
+
+    Recourse directions are generated in embedded continuous space. DataFrames
+    with categorical directions must be converted to EmbeddedDataFrames before
+    directional recourse can be generated."""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._check_types_are_numeric()
@@ -15,194 +17,133 @@ class EmbeddedDataFrame(pd.DataFrame):
     def _check_types_are_numeric(self):
         for col, dtype in zip(self.columns, self.dtypes):
             if not pd.api.types.is_numeric_dtype(dtype):
-                raise ValueError(f"Column {col} has type {dtype} which is not numeric.")
+                raise ValueError(
+                    f"Column {col} has type {dtype} which is not numeric."
+                )
         return True
 
 
 class EmbeddedSeries(pd.Series):
-    """A wrapper around Series for purely numeric data."""
+    """A wrapper around Series for continuous data.
+
+    Recourse directions are generated in embedded continuous space. Series
+    with categorical directions must be converted to EmbeddedSeries before
+    directional recourse can be generated."""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._check_types_are_numeric()
 
     def _check_types_are_numeric(self):
         if not pd.api.types.is_numeric_dtype(self.dtype):
-            raise ValueError(f"Series has type {self.dtype} which is not numeric.")
+            raise ValueError(
+                f"Series has type {self.dtype} which is not numeric."
+            )
         return True
 
 
 class RecourseAdapter(abc.ABC):
-    """An abstract base class for dataset adapters.
-    
-    The adapter translates between human-readable and embedded space.
-    """
+    """An abstract base class for recourse adapters.
+
+    The adapter translates between the original (possibly categorical) data
+    space and continuous embedded space where recourse directions are
+    generated. It also iterates recourse by converting directions in embedded
+    space to human-readable instructions and interpreting recourse instructions
+    as a hypothetical user would."""
+
     @abc.abstractmethod
     def get_label(self) -> str:
         """Gets the dataset's label column name."""
+        pass
 
     @abc.abstractmethod
     def transform(self, dataset: pd.DataFrame) -> EmbeddedDataFrame:
-        """Transforms data from human-readable format to an embedded numeric space."""
+        """Transforms data from human-readable format to an embedded continuous
+        space.
+
+        Args:
+            dataset: The data to transform.
+
+        Returns:
+            Transformed data."""
 
     @abc.abstractmethod
     def inverse_transform(self, dataset: EmbeddedDataFrame) -> pd.DataFrame:
-        """Transforms data from an embedded numeric space to its original human-readable format."""
+        """Transforms data from an embedded continuous space to its original
+        human-readable format.
+
+        Args:
+            dataset: The data to inverse transform.
+
+        Returns:
+            Inverse transformed data."""
 
     @abc.abstractmethod
     def directions_to_instructions(self, directions: EmbeddedSeries) -> Any:
-        """Converts a direction in embedded space to some human-readable instructions format."""
+        """Converts a direction in embedded space to a human-readable
+        instructions format.
+
+        Args:
+            directions: The continuous recourse directions to convert.
+
+        Returns:
+            Human-readable instructions describing the recourse directions."""
 
     @abc.abstractmethod
-    def interpret_instructions(self, poi: pd.Series, instructions: Any) -> pd.Series:
-        """Returns a new human-readable data point from an original POI and set of instructions.
-        
-        Interprets the instructions by translating the original human-readable POI."""
+    def interpret_instructions(
+        self, poi: pd.Series, instructions: Any
+    ) -> pd.Series:
+        """Returns a new human-readable data point from an original POI and set
+        of recourse instructions.
+        Interprets the instructions by translating the original human-readable
+        POI.
+
+        Args:
+            poi: The point of interest (POI) to translate.
+            instructions: The recourse instructions to interpret.
+
+        Returns:
+            A new POI translated from the original by the recourse
+            instructions."""
 
     @abc.abstractmethod
     def column_names(self, drop_label=True) -> Sequence[str]:
-        """Returns the column names of the human-readable data."""
+        """Returns the column names of the human-readable data.
+
+        Args:
+            drop_label: Whether the label column should be excluded in the
+                output.
+
+        Returns:
+            A list of the column names."""
 
     @abc.abstractmethod
     def embedded_column_names(self, drop_label=True) -> Sequence[str]:
-        """Returns the column names of the data in embedded numeric space."""
+        """Returns the column names of the data in embedded continuous space.
+
+        Args:
+            drop_label: Whether the label column should be excluded in the
+                output.
+        Returns:
+            A list of the column names."""
 
     def transform_series(self, poi: pd.Series) -> EmbeddedSeries:
-        """Transforms data from human-readable format to an embedded numeric space."""
+        """Transforms data from human-readable format to an embedded continuous
+        space.
+
+        Args:
+            dataset: The data to transform.
+
+        Returns:
+            Transformed data."""
         return self.transform(poi.to_frame().T).iloc[0]
 
     def inverse_transform_series(self, poi: EmbeddedSeries) -> pd.Series:
-        """Transforms data from an embedded numeric space to its original human-readable format."""
+        """Transforms data from an embedded continuous space to its original
+        human-readable format.
+        Args:
+            dataset: The data to inverse transform.
+
+        Returns:
+            Inverse transformed data."""
         return self.inverse_transform(poi.to_frame().T).iloc[0]
-
-
-class NaiveAdapter(RecourseAdapter):
-    """A Naive adapter which standardizes numeric columns and one hot encodes categorical columns."""
-    def __init__(self,
-                 categorical_features: Sequence[str],
-                 continuous_features: Sequence[str],
-                 perturb_ratio: Optional[float] = None,
-                 rescale_ratio: Optional[float] = None,
-                 label='Y'):
-        self.categorical_features = categorical_features
-        self.continuous_features = continuous_features
-        self.label = label
-
-        self.sc_dict: Mapping[str, StandardScaler] = None
-        self.ohe_dict: Mapping[str, OneHotEncoder] = None
-        self.columns = None
-        self.perturb_ratio = perturb_ratio
-        self.rescale_ratio = rescale_ratio
-
-    def get_label(self) -> str:
-        return self.label
-
-    def fit(self, dataset: pd.DataFrame) -> NaiveAdapter:
-        self.sc_dict = {}
-        self.ohe_dict = {}
-        self.columns = dataset.columns
-        for feature in self.continuous_features:
-            sc = StandardScaler()
-            sc.fit(dataset[[feature]])
-            self.sc_dict[feature] = sc
-        for feature in self.categorical_features:
-            ohe = OneHotEncoder()
-            ohe.fit(dataset[[feature]])
-            self.ohe_dict[feature] = ohe
-        return self
-
-    def transform(self, dataset: pd.DataFrame) -> EmbeddedDataFrame:
-        df = dataset.copy()
-        for feature in self.continuous_features:
-            if feature in df.columns:
-                df[feature] = self.sc_dict[feature].transform(df[[feature]])
-        for feature in self.categorical_features:
-            if feature in df.columns:
-                ohe = self.ohe_dict[feature]
-                feature_columns = ohe.get_feature_names_out([feature])
-                df[feature_columns] = ohe.transform(df[[feature]]).toarray()
-                df = df.drop(feature, axis=1)
-        return df
-
-    def inverse_transform(self, dataset: EmbeddedDataFrame) -> pd.DataFrame:
-        df = dataset.copy()
-        for feature in self.continuous_features:
-            if feature in df.columns:
-                df[feature] = self.sc_dict[feature].inverse_transform(df[[feature]])
-        for feature in self.categorical_features:
-            ohe = self.ohe_dict[feature]
-            feature_columns = ohe.get_feature_names_out([feature])
-            if df.columns.intersection(feature_columns).any():
-                df[feature] = ohe.inverse_transform(df[feature_columns])
-                df = df.drop(feature_columns, axis=1)
-        return df
-
-    def directions_to_instructions(self, directions: EmbeddedSeries) -> EmbeddedSeries:
-        return directions
-
-    def interpret_instructions(self, poi: pd.Series, instructions: EmbeddedSeries) -> pd.Series:
-        if self.perturb_ratio:
-            instructions = utils.randomly_perturb_dir(instructions, self.perturb_ratio)
-        if self.rescale_ratio:
-            instructions = utils.rescale_dir(instructions, self.rescale_ratio)
-        poi = self.transform_series(poi)
-        cfe = poi + instructions
-        return self.inverse_transform_series(cfe)
-
-    def column_names(self, drop_label=True) -> Sequence[str]:
-        if drop_label:
-            return self.columns.difference([self.label])
-        else:
-            return self.columns
-
-    def embedded_column_names(self, drop_label=True) -> Sequence[str]:
-        columns = self._get_feature_names_out(self.columns)
-        if drop_label:
-            return [column for column in columns if column != self.label]
-        else:
-            return columns
-
-    def _get_feature_names_out(self, features: Sequence[str]) -> Sequence[str]:
-        features_out = []
-        for feature in features:
-            if feature in self.categorical_features:
-                features_out += list(self.ohe_dict[feature].get_feature_names_out([feature]))
-            else:
-                features_out.append(feature)
-        return features_out
-
-
-class PassthroughAdapter(RecourseAdapter):
-    def __init__(self, label='Y'):
-        self.columns = None
-        self.label = label
-
-    def get_label(self) -> str:
-        return self.label
-
-    def fit(self, dataset: pd.DataFrame) -> PassthroughAdapter:
-        self.columns = dataset.columns
-        return self
-
-    def transform(self, dataset: pd.DataFrame) -> EmbeddedDataFrame:
-        return dataset
-
-    def inverse_transform(self, dataset: EmbeddedDataFrame) -> pd.DataFrame:
-        return dataset
-
-    def directions_to_instructions(self, directions: EmbeddedSeries) -> EmbeddedSeries:
-        return directions
-
-    def interpret_instructions(self, poi: pd.Series, instructions: EmbeddedSeries) -> pd.Series:
-        return poi + instructions
-
-    def column_names(self, drop_label=True) -> Sequence[str]:
-        if drop_label:
-            return self.columns.difference([self.label])
-        else:
-            return self.columns
-
-    def embedded_column_names(self, drop_label=True) -> Sequence[str]:
-        if drop_label:
-            return self.columns.difference([self.label])
-        else:
-            return self.columns
