0f6ebfb712d6d23c2a89ea03116f59f45923693e
Jake Valladares <jason.valladares3@gmail.com>
2022-11-22 16:18:26 -0500

    Added MRMC unit tests and some todos

--- recourse_methods/test_mrmc_method.py
+++ recourse_methods/test_mrmc_method.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest import mock
 import numpy as np
+import pandas as pd
 from recourse_methods import mrmc_method
 
 
@@ -14,7 +16,7 @@ volcano_alpha_small_cutoff = mrmc_method.get_volcano_alpha(
 )
 
 
-class TestUtils(unittest.TestCase):
+class TestMRMC(unittest.TestCase):
     def test_volcano_alpha_small_cutoff(self):
         distances = np.array([0, 1, 2, 3])
         weights = volcano_alpha_small_cutoff(distances)
@@ -44,3 +46,45 @@ class TestUtils(unittest.TestCase):
 
         for val, expected_val in zip(weights, expected_weights):
             self.assertAlmostEqual(val, expected_val)
+
+    @mock.patch(
+        "data.recourse_adapter.RecourseAdapter",
+        autospec=True,
+    )
+    def test_process_data(self, mock_adapter: mock.Mock):
+        mock_adapter.positive_label = 1
+        mock_adapter.label_column = "label"
+        mock_adapter.transform.side_effect = lambda df: df
+        dataset = pd.DataFrame(
+            {"col_1": [1, 2, 3], "col_2": [3, 5, 3], "label": [1, -1, 1]}
+        )
+        processed_data = mrmc_method.MRM.process_data(dataset, mock_adapter)
+        # The label should be dropped
+        self.assertNotIn("label", processed_data.columns)
+        # There should be no negative examples remaining
+        negative_label_indices = (dataset[dataset.label == -1]).index
+        self.assertEqual(
+            0, len(processed_data.index.intersection(negative_label_indices))
+        )
+        # The transform function should be called
+        mock_adapter.transform.assert_called_once()
+
+    def test_get_unnormalized_direction(self):
+        mock_self = mock.Mock(spec=["data", "alpha"])
+        poi = pd.Series([0, 0], index=["a", "b"])
+        mock_self.data = pd.DataFrame({"a": [1, 1], "b": [-1, 1]})
+        mock_self.alpha.side_effect = [pd.Series([0.75, 0.25])]
+        expected_direction = pd.Series(
+            [1, 0.75 * -1 + 0.25 * 1], index=["a", "b"]
+        )
+        direction = mrmc_method.MRM.get_unnormalized_direction(mock_self, poi)
+        for val, expected_val in zip(direction, expected_direction):
+            self.assertEqual(val, expected_val)
+
+    def test_get_unnormalized_direction_nan_alpha(self):
+        mock_self = mock.Mock(spec=["data", "alpha"])
+        poi = pd.Series([0, 0], index=["a", "b"])
+        mock_self.data = pd.DataFrame({"a": [1, 1], "b": [-1, 1]})
+        mock_self.alpha.side_effect = [pd.Series([np.nan, 0.25])]
+        with self.assertRaises(RuntimeError):
+            mrmc_method.MRM.get_unnormalized_direction(mock_self, poi)
