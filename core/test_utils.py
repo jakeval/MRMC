28b3459674c6a9f17a0bbc8f17994794267bb3bf
Jake Valladares <jason.valladares3@gmail.com>
2022-11-22 17:26:55 -0500

    minor style changes

--- core/test_utils.py
+++ core/test_utils.py
@@ -4,42 +4,44 @@ import numpy as np
 from core import utils
 
 
-MOCK_DIRECTION = np.array([0, 1])
-MOCK_RANDOM_NOISE = np.array([1, 0])
-
-
 class TestUtils(unittest.TestCase):
-    @mock.patch("core.utils.np.random.normal", return_value=MOCK_RANDOM_NOISE)
-    def test_randomly_perturb_direction(self, mock_normal):
-        new_direction = utils.randomly_perturb_direction(MOCK_DIRECTION, 1)
+    @mock.patch("core.utils.np.random.normal", return_value=np.array([1, 0]))
+    def test_randomly_perturb_direction(self, mock_normal: mock.Mock):
+        new_direction = utils.randomly_perturb_direction(np.array([0, 1]), 1)
         expected_direction = np.array([np.sqrt(0.5), np.sqrt(0.5)])
         for new_val, expected_val in zip(new_direction, expected_direction):
             self.assertAlmostEqual(new_val, expected_val)
         self.assertAlmostEqual(
-            np.linalg.norm(new_direction), np.linalg.norm(MOCK_DIRECTION)
+            np.linalg.norm(new_direction), np.linalg.norm(np.array([0, 1]))
         )
 
-    @mock.patch("core.utils.np.random.normal", return_value=MOCK_RANDOM_NOISE)
-    def test_randomly_perturb_direction_zero_ratio(self, mock_normal):
-        new_direction = utils.randomly_perturb_direction(MOCK_DIRECTION, 0)
-        expected_direction = MOCK_DIRECTION
+    @mock.patch("core.utils.np.random.normal", return_value=np.array([1, 0]))
+    def test_randomly_perturb_direction_zero_ratio(
+        self, mock_normal: mock.Mock
+    ):
+        new_direction = utils.randomly_perturb_direction(np.array([0, 1]), 0)
+        expected_direction = np.array([0, 1])
         for new_val, expected_val in zip(new_direction, expected_direction):
             self.assertAlmostEqual(new_val, expected_val)
         self.assertAlmostEqual(
-            np.linalg.norm(new_direction), np.linalg.norm(MOCK_DIRECTION)
+            np.linalg.norm(new_direction), np.linalg.norm(np.array([0, 1]))
         )
 
-    @mock.patch("core.utils.np.random.normal", return_value=MOCK_RANDOM_NOISE)
-    def test_randomly_perturb_direction_zero_direction(self, mock_normal):
+    @mock.patch("core.utils.np.random.normal", return_value=np.array([1, 0]))
+    def test_randomly_perturb_direction_zero_direction(
+        self, mock_normal: mock.Mock
+    ):
         new_direction = utils.randomly_perturb_direction(np.array([0, 0]), 0.5)
         expected_direction = np.array([0, 0])
         for new_val, expected_val in zip(new_direction, expected_direction):
             self.assertAlmostEqual(new_val, expected_val)
 
     @mock.patch("core.utils.np.random.normal", return_value=np.array([0, 0]))
-    def test_randomly_perturb_direction_zero_noise(self, mock_normal):
-        new_direction = utils.randomly_perturb_direction(MOCK_DIRECTION, 0.5)
-        expected_direction = MOCK_DIRECTION
+    def test_randomly_perturb_direction_zero_noise(
+        self, mock_normal: mock.Mock
+    ):
+        new_direction = utils.randomly_perturb_direction(np.array([0, 1]), 0.5)
+        expected_direction = np.array([0, 1])
         for new_val, expected_val in zip(new_direction, expected_direction):
             self.assertAlmostEqual(new_val, expected_val)
 
