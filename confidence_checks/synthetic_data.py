1475cba418816716b5aa546f66cc0ac66a4e5f51
Jake Valladares <jason.valladares3@gmail.com>
2022-11-25 19:55:54 -0500

    Changed the synthetic data to reflect that confidence below 0.5 is a negative classification

--- confidence_checks/synthetic_data.py
+++ confidence_checks/synthetic_data.py
@@ -17,17 +17,17 @@ _DATASET_INFO = base_loader.DatasetInfo(
 def get_data() -> Tuple[pd.DataFrame, base_loader.DatasetInfo]:
     """Generates a simple synthetic dataset:
 
-    * 1 negatively-classified POI at (0, 0).
-    * 1 positively-classified 4-point cluster at (1, 2).
-    * 1 positively-classified 4-point cluster at (-1, 2).
+    * 1 negatively-classified POI at xy-coordinates (-1, 0).
+    * 1 positively-classified 4-point cluster at xy-coords (1, 2).
+    * 1 positively-classified 4-point cluster at xy-coords (-1, 2).
 
     Returns:
         A DataFrame and DatasetInfo object.
     """
     offsets = np.array([[-0.1, -0.1], [-0.1, 0.1], [0.1, -0.1], [0.1, 0.1]])
     grid_1 = np.array([[2, 1]] * 4) + offsets
-    grid_2 = np.array([[2, -1]] * 4) + offsets
-    POI = np.array([[0, 0]])
+    grid_2 = np.array([[2, 0]] * 4) + offsets
+    POI = np.array([[0, -1]])
     data = np.concatenate([grid_1, grid_2, POI])
     labels = [1] * 8 + [-1]
     return (
