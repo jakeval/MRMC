ffccd43eaee4bc233621a7d2add3b45e3e2120b3
Jake Valladares <jason.valladares3@gmail.com>
2022-11-29 17:59:49 -0500

    Added files for running on swarm

---
+++ experiments/mrmc_experiment/swarm_launcher.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+rm result-*     # remove previous stdout and stderr logs
+
+# Arg $1: number of processes
+# Arg $2: max_runs. Optional.
+sbatch --ntasks $1 run.sbatch $1 /mnt/nfs/scratch1/$USER $2
